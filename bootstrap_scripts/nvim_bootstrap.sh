083b5771184795d0f3e387b0a4a8089032bc28b1
Rohit Mathew <rohitjm@gmail.com>
2024-08-26 20:23:49 -0500

    fix formatting

--- bootstrap_scripts/nvim_bootstrap.sh
+++ bootstrap_scripts/nvim_bootstrap.sh
@@ -7,7 +7,7 @@ LOGFILE="$DOTFILES/nvim_symlink.log"
 
 # Function to log messages with a timestamp
 log_message() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOGFILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOGFILE"
 }
 
 # Start of the script
@@ -17,18 +17,17 @@ log_message "Script started."
 log_message "Creating directories in $dst..."
 find "$src" -type d -exec mkdir -p "$dst/{}" \;
 if [ $? -eq 0 ]; then
-    log_message "Directories created successfully."
+  log_message "Directories created successfully."
 else
-    log_message "Failed to create directories."
+  log_message "Failed to create directories."
 fi
 
 # Step 2: Create symlinks for files in the corresponding destination directories
 log_message "Creating symlinks for files..."
-find "$src" -type f -exec ln -s "$DOTFILES/{}" "$dst/{}" \;
+find "$src" -type f -exec ln -sf "$DOTFILES/{}" "$dst/{}" \;
 if [ $? -eq 0 ]; then
-    log_message "Symlinks created successfully."
-else
-    log_message "Failed to create symlinks."
+  log_message "Symlinks created successfully."jjelse
+  log_message "Failed to create symlinks."
 fi
 
 # End of the script
